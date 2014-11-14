07761d59b455eae4af19560bd15a27e038a0e9e6
Пирогов Алексей <none@none>
2014-11-14 17:34:04 +0300

    moar bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y --force-yes python-dev python-pip mercurial postgresql-server-dev-9.1 python-psycopg2 build-essential
+apt-get install -y --force-yes python-dev python-pip mercurial postgresql postgresql-client postgresql-server-dev-9.1 python-psycopg2 build-essential
 easy_install -U pip
 
 echo "export BUP_PATH=/vagrant/src/barsup_demo" >> /home/vagrant/.bashrc
