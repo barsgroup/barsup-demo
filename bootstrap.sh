c11252785cc53aa6a4ca4c2b5c69a27979e61d8c
Пирогов Алексей <none@none>
2014-11-14 15:43:32 +0300

    network bypass + updated pyzmq

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y --force-yes python-pip mercurial  postgresql-server-dev-9.1 python-psycopg2 python-zmq
+apt-get install -y --force-yes python-dev python-pip mercurial postgresql-server-dev-9.1 python-psycopg2 build-essential
 easy_install -U pip
 
 echo "export BUP_PATH=/vagrant/src/barsup_demo" >> /home/vagrant/.bashrc
 
 cd /vagrant
 
-pip install -r REQUIREMENTS
+pip install --upgrade -r REQUIREMENTS
 pip install hg+https://bitbucket.org/barsgroup/barsup-core
 
