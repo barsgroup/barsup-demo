a1e532cbe1c0cf96bb0d759086c42ad502b0d728
Пирогов Алексей <none@none>
2014-11-14 16:53:03 +0300

    ext symlink creation on VM bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,8 @@ easy_install -U pip
 
 echo "export BUP_PATH=/vagrant/src/barsup_demo" >> /home/vagrant/.bashrc
 
-cd /vagrant
+cd /vagrant/src/barsup_demo/static/barsup
+ln -s ../../../../vendor/ext-5.0.1 ext
 
 pip install --upgrade -r REQUIREMENTS
 pip install hg+https://bitbucket.org/barsgroup/barsup-core
