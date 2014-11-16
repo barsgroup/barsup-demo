b51cbf90e6ab0c03a261ec39a6bbbf788243aff0
Telepenin Nikolay <telepenin@bars-open.ru>
2014-11-16 22:05:47 +0300

    Поправлен вывод сообщений

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Установка зависимых пакетов
-echo "Update & Install packages"
+echo "Updating & Installing packages"
 apt-get update
 apt-get install -y --force-yes unzip python-dev python-pip mercurial postgresql postgresql-client-9.1 postgresql-server-dev-9.1 python-psycopg2 build-essential
 easy_install -U pip
@@ -9,32 +9,32 @@ easy_install -U pip
 echo "export BUP_PATH=/vagrant/src/barsup_demo" >> /home/vagrant/.bashrc
 
 # Установка зависимостей проекта
-echo "Install project dependencies"
+echo "Installing project dependencies"
 pip install --upgrade -r /vagrant/REQUIREMENTS
 
 # Установка ExtJS
 if [ ! -d "/vagrant/src/barsup_demo/static/barsup/ext/ext-5.0.1" ]; then
     cd /vagrant/src/barsup_demo/static/barsup/ext
-    echo "Download ExtJS 5.0.1"
+    echo "Downloading ExtJS 5.0.1..."
     wget -q -o /dev/null http://cdn.sencha.com/ext/trial/ext-5.0.1-trial.zip
 
-    echo "Install ExtJS 5.0.1"
+    echo "Installing ExtJS 5.0.1..."
     unzip -qq ext-5.0.1-trial.zip
     rm ext-5.0.1-trial.zip
 fi
 
 # Создание бд и пользователи
-echo "Create user & database"
+echo "Creating user & database"
 sudo -u postgres psql -c "CREATE USER barsup WITH PASSWORD 'barsup';"
 sudo -u postgres psql -c "CREATE DATABASE barsup OWNER barsup;"
 
 # Миграции
-echo "Upgrade migrations (don't worry: red color - normal output)"
+echo "Upgrading migrations (don't worry: red color - normal output)"
 cd /vagrant/src/barsup_demo
 alembic upgrade head
 
 # Запуск торнадо для вебсокетов и воркера для синхронных задач
-echo "Start worker"
+echo "Starting worker..."
 bup_start worker >/tmp/worker.log 2>&1 &
-echo "Start server"
+echo "Starting web-server..."
 bup_start server >/tmp/server.log 2>&1 &
