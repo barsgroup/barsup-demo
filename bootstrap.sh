d2ff10b5f0ee70ab1a151487621900c7f7f4f52f
Telepenin Nikolay <telepenin@bars-open.ru>
2014-11-17 11:31:25 +0300

    Поддержка utf-8 для postgres

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Поддержка utf-8
+locale-gen en_US.UTF-8
+update-locale LANG=en_US.UTF-8 LC_CTYPE="en_US.UTF-8" LC_NUMERIC="en_US.UTF-8" LC_TIME="en_US.UTF-8" LC_COLLATE="en_US.UTF-8" LC_MONETARY="en_US.UTF-8" LC_MESSAGES="en_US.UTF-8"  LC_PAPER="en_US.UTF-8" LC_NAME="en_US.UTF-8" LC_ADDRESS="en_US.UTF-8" LC_TELEPHONE="en_US.UTF-8" LC_MEASUREMENT="en_US.UTF-8" LC_IDENTIFICATION="en_US.UTF-8" LC_ALL=en_US.UTF-8
+
 # Установка зависимых пакетов
 echo "Updating & Installing packages"
 apt-get update
@@ -26,7 +30,7 @@ fi
 # Создание бд и пользователи
 echo "Creating user & database"
 sudo -u postgres psql -c "CREATE USER barsup WITH PASSWORD 'barsup';"
-sudo -u postgres psql -c "CREATE DATABASE barsup OWNER barsup;"
+sudo -u postgres createdb -O barsup -E UTF8 -T template0 --locale=en_US.utf8 barsup
 
 # Миграции
 echo "Upgrading migrations (don't worry: red color - normal output)"
