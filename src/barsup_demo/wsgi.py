336cd8e986c56456fbdbd5e675b413ab52ab4f3d
Telepenin Nikolay <telepenin@bars-open.ru>
2014-12-10 17:32:09 +0300

    Возможность работы c uwsgi, например

--- src/barsup_demo/wsgi.py
+++ src/barsup_demo/wsgi.py
@@ -1,14 +1,13 @@
 # coding:utf-8
+import os
 
+os.environ['BUP_CONFIG'] = os.environ.get('BUP_CONFIG') or 'worker.json'
+os.environ['BUP_PATH'] = os.environ.get('BUP_PATH') or '.'
+
+from barsup.wsgi import application
 
 if __name__ == '__main__':
-    import os
-
-    os.environ['BUP_CONFIG'] = 'worker.json'
-    os.environ['BUP_PATH'] = '.'
-
     from wsgiref.simple_server import make_server
-    from barsup.wsgi import application
 
     httpd = make_server('', 8000, application)
     print("Serving on port 8000...")
