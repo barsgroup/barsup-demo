9e0f21c6d040cbedd9093f722e2b955d8a22caa7
Telepenin Nikolay <telepenin@bars-open.ru>
2014-12-10 10:51:47 +0300

    Запуск через wsgiref

--- src/barsup_demo/wsgi.py
+++ src/barsup_demo/wsgi.py
@@ -1,4 +1,15 @@
 # coding:utf-8
 
-from barsup.wsgi import application
 
+if __name__ == '__main__':
+    import os
+
+    os.environ['BUP_CONFIG'] = 'worker.json'
+    os.environ['BUP_PATH'] = '.'
+
+    from wsgiref.simple_server import make_server
+    from barsup.wsgi import application
+
+    httpd = make_server('', 8000, application)
+    print("Serving on port 8000...")
+    httpd.serve_forever()
