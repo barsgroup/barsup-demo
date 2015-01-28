ad5edc6ea77da65e167f1302587311fa6dea904d
Telepenin Nikolay <telepenin@bars-open.ru>
2015-01-28 12:03:40 +0300

    Использование локали ru_RU

--- bootstrap-wsgi.sh
+++ bootstrap-wsgi.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Поддержка utf-8
-locale-gen en_US.UTF-8
-update-locale LANG=en_US.UTF-8 LC_CTYPE="en_US.UTF-8" LC_NUMERIC="en_US.UTF-8" LC_TIME="en_US.UTF-8" LC_COLLATE="en_US.UTF-8" LC_MONETARY="en_US.UTF-8" LC_MESSAGES="en_US.UTF-8"  LC_PAPER="en_US.UTF-8" LC_NAME="en_US.UTF-8" LC_ADDRESS="en_US.UTF-8" LC_TELEPHONE="en_US.UTF-8" LC_MEASUREMENT="en_US.UTF-8" LC_IDENTIFICATION="en_US.UTF-8" LC_ALL=en_US.UTF-8
+locale-gen ru_RU.UTF-8
+update-locale LANG=ru_RU.UTF-8 LC_CTYPE="ru_RU.UTF-8" LC_NUMERIC="ru_RU.UTF-8" LC_TIME="ru_RU.UTF-8" LC_COLLATE="ru_RU.UTF-8" LC_MONETARY="ru_RU.UTF-8" LC_MESSAGES="ru_RU.UTF-8"  LC_PAPER="ru_RU.UTF-8" LC_NAME="ru_RU.UTF-8" LC_ADDRESS="ru_RU.UTF-8" LC_TELEPHONE="ru_RU.UTF-8" LC_MEASUREMENT="ru_RU.UTF-8" LC_IDENTIFICATION="ru_RU.UTF-8" LC_ALL=ru_RU.UTF-8
 
 # Установка python 3.4
 apt-get update
@@ -12,15 +12,12 @@ add-apt-repository ppa:fkrull/deadsnakes -y
 # package install/upgrade
 echo "Updating & Installing packages"
 apt-get update
-apt-get install -y --force-yes unzip python3.4 python3.4-dev python-pip mercurial postgresql postgresql-client postgresql-server-dev-all python-psycopg2 build-essential
+apt-get install -y --force-yes unzip python3.4 python3.4-dev python-pip mercurial postgresql postgresql-client postgresql-server-dev-all build-essential
 
 # installing pip
 wget https://bootstrap.pypa.io/get-pip.py -O /tmp/get-pip.py
 python3.4 /tmp/get-pip.py
 pip3.4 install --upgrade pip
-
-# installing virtualenv
-# pip3.4 install --upgrade virtualenv
 
 # BUP_PATH for SSH-sessions
 export BUP_PATH=/vagrant/src/barsup_demo
@@ -33,8 +30,6 @@ echo "BUP_SCHEMA=/vagrant/src/barsup_demo/schema.json" >> /etc/environment
 
 # project installation
 echo "Installing project dependencies"
-# virtualenv -p /usr/bin/python3.4 py3env
-# source py3env/bin/activate
 
 pip3.4 install --upgrade -r /vagrant/REQUIREMENTS
 
@@ -52,7 +47,7 @@ fi
 # Создание бд и пользователи
 echo "Creating user & database"
 sudo -u postgres psql -c "CREATE USER barsup WITH PASSWORD 'barsup';"
-sudo -u postgres createdb -O barsup -E UTF8 -T template0 --locale=en_US.utf8 barsup
+sudo -u postgres createdb -O barsup -E UTF8 -T template0 --locale=ru_RU.utf8 barsup
 
 # Миграции
 echo "Upgrading migrations (don't worry: red color - normal output)"
@@ -63,17 +58,12 @@ alembic upgrade head
 sudo -u postgres psql -d barsup -c "
 BEGIN TRANSACTION;
 
-INSERT INTO barsup.public.user (name, email, login, password) VALUES ('Administrator', 'adm@adm.ru', 'admin', 'admin');
-INSERT INTO barsup.public.role (name, is_super) VALUES ('Everything', true);
+INSERT INTO barsup.public.user (name, email, login, password) VALUES ('Администратор', 'adm@adm.ru', 'admin', 'admin');
+INSERT INTO barsup.public.role (name, is_super) VALUES ('Может все', true);
 INSERT INTO barsup.public.user_role (user_id, role_id) VALUES (1, 1);
 
 COMMIT;
 "
 echo 'For access use login "admin" and password "admin"'
 
-# installation/start of daemon configurations
-export PYTHONPATH=/vagrant/src:$PYTHONPATH
-export PYTHONPATH=/usr/lib/python3.4/site-packages:$PYTHONPATH
 
-# uwsgi --uid=vagrant --daemonize =/tmp/uwsgi.log wsgi.ini
-python3.4 wsgi.py
